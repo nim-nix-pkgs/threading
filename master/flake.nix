7b59831f09e9ea5eca4dd8471a264b6ee3c8ccbf
hugosenari <hugosenari@gmail.com>
2022-03-12 03:34:31 -0300

    chore: re index threading

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''New atomics, thread primitives, channels and atomic refcounting for --gc:arc/orc.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-threading-master.flake = false;
+  inputs.src-threading-master.owner = "nim-lang";
+  inputs.src-threading-master.ref   = "refs/heads/master";
+  inputs.src-threading-master.repo  = "threading";
+  inputs.src-threading-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-threading-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-threading-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
